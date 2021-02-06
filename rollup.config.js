b0fc0f2c23458bfb3269a55f6d2c98e46fc1bdfa
CalmDownVal <vaclav.lahuta@gmail.com>
2021-02-06 20:26:51 +0100

    configure rollup build

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import typescript from '@rollup/plugin-typescript';
+import definitions from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+	{
+		input: './src/index.ts',
+		output: [
+			{
+				file: './build/index.cjs.min.js',
+				format: 'cjs'
+			},
+			{
+				file: './build/index.esm.min.js',
+				format: 'esm'
+			}
+		],
+		external: [
+			'@calmdownval/signal',
+			'crypto',
+			'http',
+			'https',
+			'url'
+		],
+		plugins: [
+			typescript(),
+			terser({
+				output: {
+					comments: false
+				}
+			})
+		]
+	},
+	{
+		input: './src/index.ts',
+		output: {
+			file: './build/index.d.ts',
+			format: 'es'
+		},
+		external: [
+			'@calmdownval/signal',
+			'tls'
+		],
+		plugins: [
+			definitions()
+		]
+	}
+];
